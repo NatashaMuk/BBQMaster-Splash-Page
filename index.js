ce874a9fd58d94032f37d18ee9fd20d19dbcf026
NatM <99290694+NatashaMuk@users.noreply.github.com>
2022-05-11 21:59:34 +0100

    timeout function added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const emailFormCollector = document.getElementById("email-collector");
 const container = document.querySelector(".container");
+const ctaSection = document.getElementById("cta-section");
 const registerBtn = document.querySelector(".btn-hero");
 const exitBtn = document.querySelector(".exit-btn");
+const exitIcon = document.querySelector(".exit-icon");
 
 const toggleLayout = function () {
   container.classList.toggle("transition");
@@ -20,6 +22,6 @@ emailFormCollector.addEventListener("submit", (event) => {
   <p class="cta-subtitle">You are on your way to becoming<br>a BBQ Master!</p>
   <p class="privacy-info">Further information will be sent to ${email}</p>
 `;
-  const ctaSection = document.getElementById("cta-section");
   ctaSection.innerHTML = updateHtmlContent;
+  setTimeout(toggleLayout, 4000);
 });
