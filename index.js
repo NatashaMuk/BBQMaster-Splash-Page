a359105a73240827994db012bfdef2af8c185b47
NatM <99290694+NatashaMuk@users.noreply.github.com>
2022-05-11 20:39:26 +0100

    anonymous function removed on event listener
    anonymous function on button click removed.  a reusable Function expression added.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const emailFormCollector = document.getElementById("email-collector");
 const container = document.querySelector(".container");
 const registerBtn = document.querySelector(".btn-hero");
+const exitBtn = document.querySelector(".exit-btn");
 
-registerBtn.addEventListener("click", function () {
+const toggleLayout = function () {
   container.classList.toggle("transition");
-});
+};
+registerBtn.addEventListener("click", toggleLayout);
+exitBtn.addEventListener("click", toggleLayout);
 
 emailFormCollector.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -14,7 +17,7 @@ emailFormCollector.addEventListener("submit", (event) => {
   const email = emailFormData.get("email");
 
   const updateHtmlContent = `<h2>Congratulations ${userFirstName} </h2>
-  <p class="cta-subtitle">You are on your way to becoming a BBQ Master</p>
+  <p class="cta-subtitle">You are on your way to becoming<br>a BBQ Master!</p>
   <p class="privacy-info">Further information will be sent to ${email}</p>
 `;
   const ctaSection = document.getElementById("cta-section");
